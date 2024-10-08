f8db6326c7490d64c643f6f1951244d72ba1900e
ShotikOFF86 <ledidog2000@gmail.com>
2024-10-08 12:10:26 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,6 @@
+completed_homework = 12
+hours_spent = 1.5
+course_name = "Python"
+time_for_one_task = hours_spent / completed_homework
+print(completed_homework, hours_spent, course_name, time_for_one_task)
+
